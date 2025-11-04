c57ca45b1f5d902ef7b13b831f1fa77585d431ac
Kapil <kapilraut026@gmail.com>
2025-11-04 23:07:06 +0545

    hello

--- Casplit-main/casplit-backend/server.js
+++ Casplit-main/casplit-backend/server.js
@@ -26,10 +26,7 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
-// const connectionString = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/casplit?authSource=admin`
 const connectionString = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@xnap.e3ov2nj.mongodb.net/?appName=xnap`;
-
-// const connectionString = `mongodb+srv://rautkapil124_db_user:aaKOcJuK9CGFUbfV@xnap.e3ov2nj.mongodb.net/?appName=xnap`;
 
 mongoose
   .connect(
@@ -53,15 +50,15 @@ app.use("/api/users", usersRouter);
 app.use("/api/group", apiAuth.validateToken, gorupRouter);
 app.use("/api/expense", apiAuth.validateToken, expenseRouter);
 
-// if (
-//   process.env.NODE_ENV === "production" ||
-//   process.env.NODE_ENV === "staging"
-// ) {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //To detect and log invalid api hits
 app.all("*", (req, res) => {
